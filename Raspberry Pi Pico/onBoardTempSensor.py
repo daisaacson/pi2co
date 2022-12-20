2f29068543324a963586d21f038b03e6e806af8c
daisaacson <49328302+daisaacson@users.noreply.github.com>
2022-12-19 23:33:53 -0600

    learning on board pi Pico temp sensor

---
+++ Raspberry Pi Pico/onBoardTempSensor.py
@@ -0,0 +1,32 @@
+from machine import ADC
+import time
+
+temperatureBuffer = []
+temperatureBufferLength = 10
+
+temperatureSensor = machine.ADC(4)
+conversionFactor = 3.3 / 65535
+
+sleepSeconds = 5
+
+def toFahrenheit(raw):
+    # convert raw temp sensor reading to Fahrenheit
+    return 32 + ( 1.8 * ( 27 - ((raw * conversionFactor) - 0.706)/0.001721 ) )
+
+def toCelcius(raw):
+    # convert raw temp sensor reading to Celcius
+    return 27 - ((raw * conversionFactor) - 0.706)/0.001721
+
+while True:
+    # read raw temperature from sensor
+    temperatureReadingRaw = temperatureSensor.read_u16()
+    
+    # Store raw vaules in a FIFO buffer to level out temperature readings
+    temperatureBuffer.append(temperatureReadingRaw)
+    if len(temperatureBuffer) > temperatureBufferLength:
+        temperatureBuffer.pop(0)
+        
+    # Print averaged temperature reading
+    print ("Temperature: {0:.1f}°F".format(toFahrenheit(sum(temperatureBuffer)/len(temperatureBuffer))))
+
+    time.sleep(sleepSeconds)
